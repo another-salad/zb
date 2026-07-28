9c3cca8f3a0c1b26c1db0e8728f4a921febe6b99
AnotherSalad <48966874+another-salad@users.noreply.github.com>
2026-07-28 14:20:14 +0100

    presence and friends

--- scripts/triggerlights/trigger.ps1
+++ scripts/triggerlights/trigger.ps1
@@ -2,7 +2,8 @@ param(
     [Parameter(Mandatory)][string]$Hostname,
     [int]$MaximumLightBrightness = 255,  # Max brightness to set lights to when triggered.
     [string[]]$OnOffOnlyGroups,  # Unfortunately I cannot find a way of detecting what a group supports via the API.
-    [Parameter(Mandatory)][string[]]$ModulesToImport
+    [Parameter(Mandatory)][string[]]$ModulesToImport,
+    [switch]$Block
 )
 
 $EventName = [PSCustomObject]@{
@@ -14,7 +15,63 @@ Register-EngineEvent -SourceIdentifier $EventName.Presence -Action {
     $Event.MessageData.ModulesToImport | % {import-module $_ -Force}
     $manager = [GroupEvent.GroupManager]::new()
     New-ConbeeSessionUsingVault -hostname $Event.MessageData.Hostname
-    # Write-Host "Received presence event for sensor: $($Event.MessageData | ConvertTo-Json -Depth 3)"
+    $darknessEventType = 423
+    $darknessLockOffset = $manager.GetPowerEventOffset($darknessEventType)  # $manager.NewPowerEventOffset(423, (New-TimeSpan -Hours 12))
+    $Event.MessageData.TriggerSensors | Where-Object { [int]$_.apiid -eq [int]$Event.MessageData.sensorEvent.id } | % {
+        $_.TriggerGroup | % {
+            $TriggerGroup = $_
+            $Group = Get-GroupByName -Name $TriggerGroup  # Gets the group state from the API
+            $Group | Add-Member -MemberType NoteProperty -Name SupportsBrightness -Value $(if ($Group.Name -in $Event.MessageData.OnOffOnlyGroups) {$False} else {$True}) -Force
+            # So we will have multiple sensors in one area, as we walk through said area each sensor will be processed. We don't want subsequent states
+            # from tripping us up. So we have to consider if the group has already been processed due to an earlier (but still _current_) event.
+            # To do this we will have the concept of effective darkness. If presence is detected and we are effectively in darkness (i.e. its dark, or sensors are set to ignore light levels)
+            # we will always want to turn them on, the inverse for no presence detected.
+            $GroupSensors = $Event.MessageData.TriggerSensors | Where-Object { $TriggerGroup -in $_.TriggerGroup }
+            $LiveSensorState = Get-PresenceSensors | Where-Object { $_.ApiId -in $GroupSensors.ApiId }  # Api call
+            $PresenceDetected = Test-AnySensorProperty -Sensors $LiveSensorState -Predicate { $_.state.presence }
+            $CurrentLock = $manager.GetGroupLock($Group.Id) 
+            if ($CurrentLock) {
+                # TODO: Fix this horror up.
+                # I hadn't initially intended for offsets to be so closely linked to requesttypes, but in practice it has turned out this way.
+                # There were reasons for the offset table to be so loosey goosey when I was implementing it, but it was too long ago for me to remember right now.
+                # Needs a bit of a shape change here.
+                if ($CurrentLock.RequestType -eq $darknessEventType -and !$PresenceDetected) {
+                    # We have set this to avoid subsequent detection events from fighting eachother whilst someone is in a room.
+                    # However the room is now empty, so kill the lock.
+                    Write-Debug "Nuking _darkness_ lock for '$($Group.name)' due to no presence being detected"
+                    $manager.RemoveGroupLock($Group.Id)
+                }
+                # Currently for all non-darkness events, may end up doing button specific events later on.
+                elseif ($CurrentLock.ReleaseTime -le (Get-Date -AsUTC)) {
+                    # Lock has expired, kill it. We'll continue with the usual presence based setting/unsetting flow below.
+                    Write-Debug "Nuking lock for '$($Group.name)' due to time expiry"
+                    $manager.RemoveGroupLock($Group.Id)
+                } else {
+                    # Group is locked, move on fam.
+                    return
+                }
+            }
+
+            $LightGroupState = $Group | New-LightGroupState -transitiontime 10
+            if ($Group.SupportsBrightness) {
+                $LightGroupState.Bri = $Event.MessageData.MaximumLightBrightness
+            }
+
+            $IgnoreDaylightSetting = Test-AnySensorProperty -Sensors ($Event.MessageData.TriggerSensors | Where-Object { $TriggerGroup -in $_.TriggerGroup }) -Predicate { $_.IgnoreDaylight }
+            if ($PresenceDetected -and ((Test-AnySensorProperty -Sensors $LiveSensorState -Predicate { $_.state.dark }) -or ($IgnoreDaylightSetting -or (-not (Get-DaylightSensors -IgnoreFilter).state.daylight)))) {
+                # It is _dark_ (right hand side of the above if) and we have detected someone.
+                # Set a group lock with the darkness lock event value, adding a large offset for just some form of safety really.
+                Write-Debug "Presence detected for group $($Group.Name), ignore daylight setting: $IgnoreDaylightSetting"
+                $manager.NewGroupLock([GroupEvent.GroupLockDTOWithOffset]::new([int]$Group.Id,$Group.Name,$darknessEventType,[GroupEvent.PowerState]::On,$darknessLockOffset))
+            } else {
+                # LightGroup default state is $MaximumLightBrightness, so just turn them off if we are in _effective_ daylight.
+                Write-Debug "Turning group: $($Group.name) off"
+                $LightGroupState.Bri = $null
+                $LightGroupState.On = $false
+            }
+            $LightGroupState | Set-LightGroupState
+        }
+    }
 }
 
 Register-EngineEvent -SourceIdentifier $EventName.ButtonEvent -Action {
@@ -31,7 +88,7 @@ Register-EngineEvent -SourceIdentifier $EventName.ButtonEvent -Action {
             }
             # Super speedy check if there are any presence sensors in our group before doing too much other work.
             if (!($Event.MessageData.TriggerSensors | Where-Object { $TriggerGroup -in $_.TriggerGroup } | where type -eq ZHAPresence)) {
-                Write-Information "No presence sensors associated with group $($Group.name). Button press authoritative. No state locking required."
+                Write-Debug "No presence sensors associated with group $($Group.name). Button press authoritative. No state locking required."
                 if ($Group.state.any_on) {
                     $LightGroupState.Bri = $null
                     $LightGroupState.On = $false
@@ -42,26 +99,27 @@ Register-EngineEvent -SourceIdentifier $EventName.ButtonEvent -Action {
             } else {
                 $manager = [GroupEvent.GroupManager]::new()
                 $buttonState = [int]$Event.MessageData.sensorEvent.state.buttonevent
-                Write-Information "Button event: $buttonState"
                 $ButtonOverride = $manager.GetPowerEventOffset($buttonState)
                 if (!$ButtonOverride) {
                     Write-Error "Unknown button event state: $buttonState. I can offer you nothing."
                 } else {
-                    if (!$manager.GetGroupLock($Group.Id)) {
+                    $currentLock = $manager.GetGroupLock($Group.Id)
+                    if (!$currentLock) {
                         # We are wanting to lock a light on
-                        Write-Information "Locking group: $($Group.Name) on for + $($ButtonOverride.Offset)"
+                        Write-Debug "Locking group: $($Group.Name) on for + $($ButtonOverride.Offset)"
                         $manager.NewGroupLock(
                             [GroupEvent.GroupLockDTOWithOffset]::new([int]$Group.Id,$Group.Name,[int]$ButtonOverride.Name,[GroupEvent.PowerState]::On,$ButtonOverride)
                         )
-                        $LightGroupState | Set-LightGroupState
-                        if ($Group.state.any_on) {  # Old value prior to button press.
-                            # Lights were on prior, so we should acknowledge that a lock has been set.
-                            $LightGroupState | Set-LightAcknowledge -OnOffOnly:(!$Group.SupportsBrightness)
-                        }
+                    } elseif ($currentLock.RequestType -eq 423){
+                        # Light locked on by presence event, override due to button event and update offset time
+                        Write-Debug "Overriding existing presence lock for: $($Group.Name) with new button event: $($ButtonOverride.Offset)"
+                        $manager.SetGroupLock(
+                            [GroupEvent.GroupLockDTOWithOffset]::new([int]$Group.Id,$Group.Name,[int]$ButtonOverride.Name,[GroupEvent.PowerState]::On,$ButtonOverride)
+                        )
                     } else {
                         $IgnoreDaylightSetting = Test-AnySensorProperty -Sensors ($Event.MessageData.TriggerSensors | Where-Object { $TriggerGroup -in $_.TriggerGroup }) -Predicate { $_.IgnoreDaylight }
                         $manager.RemoveGroupLock($Group.Id)
-                        Write-Information "Group $($Group.name) unlocked"
+                        Write-Debug "Group $($Group.name) unlocked"
                         # If its dark, let the presence sensor take over. But we should at least do a cheeky flicker so we know the lock has been killed.
                         if (-not ((Get-DaylightSensors -IgnoreFilter).state.Daylight) -or ($IgnoreDaylightSetting -and $group.state.any_on)) {
                             # If its dark or the group ignores daylight and is on, flicker the lights to show we are unlocking.
@@ -72,6 +130,12 @@ Register-EngineEvent -SourceIdentifier $EventName.ButtonEvent -Action {
                             $LightGroupState.On = $false
                             $LightGroupState | Set-LightGroupState
                         }
+                        return  # as we have a unique light setting case
+                    }
+                    $LightGroupState | Set-LightGroupState
+                    if ($Group.state.any_on) {  # Old value prior to button press.
+                        # Lights were on prior, so we should acknowledge that a lock has been set.
+                        $LightGroupState | Set-LightAcknowledge -OnOffOnly:(!$Group.SupportsBrightness)
                     }
                 }
             }
@@ -79,10 +143,11 @@ Register-EngineEvent -SourceIdentifier $EventName.ButtonEvent -Action {
     }
 }
 
-
+trap {
+    get-job | stop-job
+}
 
 start-job -name LightManager -scriptblock {
-    $InformationPreference = "Continue"
     $using:ModulesToImport | % {import-module $_ -Force}
     New-ConbeeSessionUsingVault -hostname $using:Hostname
     $ws = New-WsConnection
@@ -104,7 +169,7 @@ start-job -name LightManager -scriptblock {
                     MaximumLightBrightness = $using:MaximumLightBrightness
                 }
                 # We care about buttonevents or presence updates, generic state changed events can be dropped to the floor
-                $EventType = if ($sensorEvent.state.ButtonEvent) { $using:EventName.ButtonEvent } elseif ($sensorEvent.state.Presence) { $using:EventName.Presence } else { $null }
+                $EventType = if ($sensorEvent.state.ButtonEvent) { $using:EventName.ButtonEvent } elseif ($sensorEvent.state | gm -name Presence) { $using:EventName.Presence } else { $null }
                 if ($EventType) {
                     New-Event -SourceIdentifier $EventType -MessageData $EventData 
                 }
@@ -113,4 +178,8 @@ start-job -name LightManager -scriptblock {
     } finally {
         $ws | Close-WsConnection
     }
+}
+
+if ($Block) {
+    get-job | wait-job
 }
