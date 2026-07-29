77542e2234e7a792d1eedb474fbf90730d046343
AnotherSalad <48966874+another-salad@users.noreply.github.com>
2026-07-29 23:15:02 +0100

    un-comment line

--- scripts/setup/db.ps1
+++ scripts/setup/db.ps1
@@ -6,7 +6,7 @@ param(
 Import-module $DllPath -Force
 
 $manager = [GroupEvent.GroupManager]::new()
-#$manager.NewPowerEventOffset(423, (New-TimeSpan -Hours 12)) # _darkness_ state event offset
+$manager.NewPowerEventOffset(423, (New-TimeSpan -Hours 12)) # _darkness_ state event offset
 $manager.NewPowerEventOffset(1002, (New-TimeSpan -Hours 1)) # Button short
 $manager.NewPowerEventOffset(1003, (New-TimeSpan -Hours 8)) # Button long
 $manager.NewPowerEventOffset(1004, (New-TimeSpan -Hours 2)) # Double Press
